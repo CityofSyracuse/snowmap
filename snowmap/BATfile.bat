abcf2479e2c0a6c7e35978c603130d1086007912
samedelstein <Syr2021@github.com>
2020-12-17 13:19:17 -0500

    Update the map

--- snowmap/BATfile.bat
+++ snowmap/BATfile.bat
@@ -2,19 +2,9 @@ conda activate exe
 
 python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\clearGPSdata.py"
 
-python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\vehicleList.py"
-
-python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\Snowplow_mapping_geojson_getting_started.py"
-
-python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\Snowplow_mapping_geojson.py"
-
 python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\pullGPSdata.py"
 
-python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\odometer_readings.py"
-
 python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\create_polygons.py"
-
-python "C:\Users\adarc\Desktop\City of Syracuse\snowmap-master\snowmap\snowmap\snowmap\create_lines_map.py"
 
 #!/bin/bash
 cd Desktop/City of Syracuse/snowmap-master/snowmap/snowmap
