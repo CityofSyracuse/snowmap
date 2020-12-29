be696a988b88f51c8e8f0328953f52f51aff5533
samedelstein <Syr2021@github.com>
2020-12-29 00:18:23 -0500

    Update the map

--- snowmap/pullGPSdata.py
+++ snowmap/pullGPSdata.py
@@ -122,7 +122,7 @@ timezone = timedelta(hours = 4)
 
 
 systime = datetime.now() + timezone
-start_date = (systime - timedelta(minutes=180)).strftime("%Y-%m-%dT%H:%M:%SZ")
+start_date = (systime - timedelta(minutes=3880)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 # In[144]:
